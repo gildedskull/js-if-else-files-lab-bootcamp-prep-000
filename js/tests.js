4aaa0707646ec14bc5eed3ba8d2f9481155a568d
Matthew M <mbmoorin@gmail.com>
2020-06-07 00:11:45 +0000

    Automatically backed up by Learn

--- js/tests.js
+++ js/tests.js
@@ -1,4 +1,4 @@
-var index = require("./index.js")
+
 
 if (index.name === "Susan") {
     console.log("The name is correct")
@@ -13,3 +13,4 @@ if (index.height === 70) {
 } else {
     console.log("Expected: 70, Received: "+index.height)
 }
+//Cannot figure out why "The name is correct" prints to the console twice even when I remove var index....
