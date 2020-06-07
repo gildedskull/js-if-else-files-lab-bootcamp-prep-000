e1a537c407332053bad11dc2936b8ec9636ba3bd
Matthew M <mbmoorin@gmail.com>
2020-06-07 00:12:35 +0000

    Automatically backed up by Learn

--- js/tests.js
+++ js/tests.js
@@ -13,4 +13,4 @@ if (index.height === 70) {
 } else {
     console.log("Expected: 70, Received: "+index.height)
 }
-//Cannot figure out why "The name is correct" prints to the console twice even when I remove var index....
+//Cannot figure out why "The name is correct" prints to the console twice..
